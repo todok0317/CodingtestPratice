d8e9badc4af409fddf5d338265b9ccd0b20b0d2a
todok0317 <todok0317@naver.com>
2025-07-30 11:52:59 +0900

    2025.07.30 프로그래머스 K번째 수

---
+++ src/codingtest/programmers/KthNumber.java
@@ -0,0 +1,32 @@
+package codingtest.programmers;
+
+import java.util.Arrays;
+
+public class KthNumber {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        KthNumber solver = new KthNumber();
+
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {
+            {2, 5, 3},
+            {4, 4, 1},
+            {1, 7, 3}
+        };
+
+        int[] result = solver.solution(array, commands);
+        System.out.println(Arrays.toString(result));  // [5, 6, 3]
+    }
+}
+
