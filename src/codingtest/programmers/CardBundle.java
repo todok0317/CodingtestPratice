e2948768a3f5d7d84c5cd35819a73141f41846cd
todok0317 <todok0317@naver.com>
2025-08-02 20:41:19 +0900

    2025.08.02 프로그래머스 카드 뭉치

---
+++ src/codingtest/programmers/CardBundle.java
@@ -0,0 +1,34 @@
+package codingtest.programmers;
+
+public class CardBundle {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int idx1 = 0; // cards1 배열의 인덱스
+        int idx2 = 0; // cards2 배열의 인덱스
+
+        // goal 배열 순회
+        for (String currentStr : goal) {
+            // currentStr이 cards1[idx1] 또는 cards2[idx2]에 포함하는지 확인
+            if (idx1 < cards1.length && currentStr.equals(cards1[idx1])) {
+                idx1++; // cards1[idx1]에 포함하는 경우 idx1 증가
+            } else if (idx2 < cards2.length && currentStr.equals(cards2[idx2])) {
+                idx2++; // cards2[idx2]에 포함하는 경우 idx2 증가
+            } else {
+                return "No"; // 둘 다 포함하지 않는 경우
+            }
+        }
+
+        return "Yes";
+    }
+
+    public static void main(String[] args) {
+        CardBundle cardBundle = new CardBundle();
+
+        String[] cards1 = {"i", "drink", "water"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+
+        String result = cardBundle.solution(cards1, cards2, goal);
+        System.out.println(result);  // 출력: Yes
+    }
+}
+
